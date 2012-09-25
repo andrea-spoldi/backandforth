e40a4171e83a30a52b8395d99ca9942f3f89d8a1
andrea spoldi <andrea.spoldi@gmail.com>
2012-09-25 13:00:38 +0000

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backandforth::Application.config.secret_token = 'a14adba0a75edca9f8d7619cce60d5453031597db2d040dcde8a595c719a3120a3faa5f1cfa4e5c45e076aba24a014125572a6890b0e99cab2f76cb8d848a840'
